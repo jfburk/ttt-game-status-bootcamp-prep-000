a1b9b55a680f49be64ed760bc9832d519e1fda5e
jfburk <cineburk@gmail.com>
2018-06-08 04:04:16 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -9,6 +9,9 @@ WIN_COMBINATIONS = [
   [0,1,2], #Top row
   [3,4,5], #Middle row
   [6,7,8], #Bottom row
+  [0,3,6], #Left row
+  [1,4,7], #Middle Row Down
+  [2,5,8], #Right row
   [0,4,8], #Diagonal 1
   [2,4,6], #Diagonal 2
 ]
